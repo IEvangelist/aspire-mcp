51c79d0c75a09efd0bfab2e270ba668e22af8cfb
David Pine <david.pine@microsoft.com>
2025-04-21 21:51:28 -0500

    Requires clean up

--- AspireMcp.Server/obj/Debug/net9.0/AspireMcp.Server.AssemblyInfo.cs
+++ AspireMcp.Server/obj/Debug/net9.0/AspireMcp.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AspireMcp.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1fe4300bbbbc8f5868215317d009b9bd4da6577")]
 [assembly: System.Reflection.AssemblyProductAttribute("AspireMcp.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AspireMcp.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
